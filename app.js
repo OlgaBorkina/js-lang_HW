546d4bfdf771a4c7d3f164ae3327eb073e29c130
OlgaBorkina <ovborkina@gmail.com>
2022-01-25 21:06:50 +0200

    HW # 14_1

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
 function createAddress(city, street) {
-    //{city: city, street: street} <=> {city, street}
-    return {city, street}
+        return {city, street}
 }
 function createPerson(id, name, address) {
     return {id, name, address};
 }
 const persons = [
-    createPerson(123, "Vasya", createAddress("Rehovot","Parshani")),
-    createPerson(124, "Olya", createAddress("Rehovot","Pr. Plaut")),
+    createPerson(123, "Vasya", createAddress("REHOVot","Parshani")),
+    createPerson(124, "Olya", createAddress("rEhOvOT","Pr. Plaut")),
     createPerson(125, "Tolya", createAddress("Tel-Aviv","Dizengoff")),
     createPerson(126, "Sara", createAddress('Lod', 'Sokolov'))
 ]
 
-const getPersonsCity = persons.filter(people => people.address.city=='Rehovot');
+const getPersonsCity = persons.filter(people => people.address.city.toLowerCase()==='rehovot');
 console.log(`+++++++ Task 3 +++++++++`);
 console.log (`Peoples from Rehovot `,getPersonsCity);
-
-
 
 const newPersonsSort =  persons.slice();
  newPersonsSort.sort((a,b) => {
