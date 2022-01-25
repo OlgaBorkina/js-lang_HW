4e12111d24ed2e96dd7f66b1664d3bfeff42e0b4
OlgaBorkina <ovborkina@gmail.com>
2022-01-25 23:10:33 +0200

    HW # 14_1

--- app.js
+++ app.js
@@ -5,20 +5,26 @@ function createPerson(id, name, address) {
     return {id, name, address};
 }
 const persons = [
-    createPerson(123, "Vasya", createAddress("REHOVot","Parshani")),
-    createPerson(124, "Olya", createAddress("rEhOvOT","Pr. Plaut")),
+    createPerson(124, "Olya", createAddress("Rehovot","Pr. Plaut")),
     createPerson(125, "Tolya", createAddress("Tel-Aviv","Dizengoff")),
+    createPerson(123, "Vasya", createAddress("Rehovot","Parshani")),
     createPerson(126, "Sara", createAddress('Lod', 'Sokolov'))
 ]
 
-const getPersonsCity = persons.filter(people => people.address.city.toLowerCase()==='rehovot');
+let city = '               tela viv'
+const getPersonsCity = persons.filter(people => 
+    people.address.city.replace(/[ -]+/g, '').toLowerCase()===city.replace(/[ -]+/g, '').toLowerCase());
 console.log(`+++++++ Task 3 +++++++++`);
-console.log (`Peoples from Rehovot `,getPersonsCity);
+console.log (`Peoples from ${city} `,getPersonsCity);
 
 const newPersonsSort =  persons.slice();
  newPersonsSort.sort((a,b) => {
-       if (a.address.city === 'Rehovot') return 1;
-       if (a.address.city !== 'Rehovot') return -1;
+        if (a.address.city.replace(/[ -]+/g, '').toLowerCase() === city.replace(/[ -]+/g, '').toLowerCase() 
+        && b.address.city.replace(/[ -]+/g, '').toLowerCase() !== city.replace(/[ -]+/g, '').toLowerCase()) return 1;
+        if (a.address.city.replace(/[ -]+/g, '').toLowerCase() !== city.replace(/[ -]+/g, '').toLowerCase() 
+        && b.address.city.replace(/[ -]+/g, '').toLowerCase() === city.replace(/[ -]+/g, '').toLowerCase()) return -1;
+    //    if (a.address.city === 'Rehovot' && b.address.city !== 'Rehovot') return 1;
+    //    if (a.address.city !== 'Rehovot' && b.address.city === 'Rehovot') return -1;
       return 0;
      });
 console.log(`+++++++ Task 4 +++++++++`);   
