a0bbfc8cc5257bc67a986b5cb7e686241af9a10f
OlgaBorkina <ovborkina@gmail.com>
2022-01-25 20:50:01 +0200

    HW #14

--- app.js
+++ app.js
@@ -1,37 +1,29 @@
-
-/********************HW 11 definition */
-function encode(num, codingString) {
-    let r = checkCS(codingString);
-    let res = "";
-    let number = num;
-    if (r == "ok"){
-        let codestr="" + codingString;
-        do {
-                const digit = Math.trunc(num % codestr.length);
-                const symb = getSymbol(digit, codestr);
-                res = symb + res;
-                num = Math.trunc(num / codestr.length);
-        } while(num >= 1);
-    return console.log(`Result of encoding ` + number + ` to "` + codingString + `" notation is - ` + res);
-//    return res;
-    }
+function createAddress(city, street) {
+    //{city: city, street: street} <=> {city, street}
+    return {city, street}
 }
-
-function getSymbol(digit, codingString) {
-    const strP = "" + codingString;
-    let symbol= +digit;
-return "" + strP[symbol]; 
+function createPerson(id, name, address) {
+    return {id, name, address};
 }
+const persons = [
+    createPerson(123, "Vasya", createAddress("Rehovot","Parshani")),
+    createPerson(124, "Olya", createAddress("Rehovot","Pr. Plaut")),
+    createPerson(125, "Tolya", createAddress("Tel-Aviv","Dizengoff")),
+    createPerson(126, "Sara", createAddress('Lod', 'Sokolov'))
+]
 
-function checkCS (codingString){
-    const strP = "" + codingString;
-        if (strP.length < 2) return (console.log('Error - Code String too short'));
-        else {
-            for (let i=0; i<strP.length-1; i++){
-                if (strP.substring(i+1, strP.length).includes (strP[i])) return console.log('Error - repeated symbol - ' + (strP[i]) );
-            }
-        }
-return "ok";
-}
+const getPersonsCity = persons.filter(people => people.address.city=='Rehovot');
+console.log(`+++++++ Task 3 +++++++++`);
+console.log (`Peoples from Rehovot `,getPersonsCity);
 
-console.log(encode(328, "!@#$%"));
+
+
+const newPersonsSort =  persons.slice();
+ newPersonsSort.sort((a,b) => {
+       if (a.address.city === 'Rehovot') return 1;
+       if (a.address.city !== 'Rehovot') return -1;
+      return 0;
+     });
+console.log(`+++++++ Task 4 +++++++++`);   
+console.log(newPersonsSort);
+  
